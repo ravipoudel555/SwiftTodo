e09eeb5e154bbbd7a1ee856e294af9bc3d2c91c3
Ravi Poudel <ravi.poudel@nexdigitalstudio.com>
2025-09-23 18:51:50 +0545

    Initial Commit

---
+++ SwiftTodo/SwiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoApp.swift
+//  SwiftTodo
+//
+//  Created by nex on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
